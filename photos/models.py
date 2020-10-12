dc6b474e1205d77b7215a081964aa16f2d29b353
clicktronix <s28.morgan@gmail.com>
2020-10-12 23:48:05 +0600

    Migrations added

--- photos/models.py
+++ photos/models.py
@@ -6,8 +6,8 @@ class Photo(models.Model):
     """Photo model class"""
 
     created = models.DateTimeField(auto_now_add=True)
-    width = models.IntegerField(max_length=10)
-    height = models.IntegerField(max_length=10)
+    width = models.IntegerField()
+    height = models.IntegerField()
     src = models.CharField(max_length=100)
     album = models.TextField(max_length=100)
     is_grid = models.BooleanField(default=False)
