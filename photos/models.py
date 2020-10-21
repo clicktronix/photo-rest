5e53c0a00081cd6ba9915f7cd482746ec8b1ebf8
clicktronix <s28.morgan@gmail.com>
2020-10-21 23:39:30 +0600

    Changed photo model

--- photos/models.py
+++ photos/models.py
@@ -6,7 +6,9 @@ class Photo(models.Model):
     """Photo model class"""
 
     created = models.DateTimeField(auto_now_add=True)
-    width = models.IntegerField()
-    height = models.IntegerField()
-    img = models.ImageField(upload_to="photos", max_length=255, blank=True)
+    width = models.PositiveIntegerField(editable=False)
+    height = models.PositiveIntegerField(editable=False)
+    img = models.ImageField(
+        upload_to="photos", max_length=255, height_field="height", width_field="width"
+    )
     is_grid = models.BooleanField(default=False)
