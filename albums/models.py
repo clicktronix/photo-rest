c081e8c1aa199ac59973db7466e768fbf0821c5c
clicktronix <s28.morgan@gmail.com>
2021-03-20 13:25:03 +0600

    Photo models fields updatedd

--- albums/models.py
+++ albums/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Album(models.Model):
     """Album model class"""
 
-    name = models.CharField(max_length=120, blank=False, null=False)
-    description = models.TextField(max_length=300, null=True)
+    name = models.CharField(max_length=120, blank=False)
+    description = models.TextField(max_length=300, blank=True)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
 
